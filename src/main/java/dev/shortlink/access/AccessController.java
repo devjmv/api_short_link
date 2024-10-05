1e9b221e928a57c2aef0c94739252823cbc32bce
devjmv <javier161890@gmail.com>
2024-10-05 10:06:10 +0200

    added controller to redirect to link

--- src/main/java/dev/shortlink/access/AccessController.java
+++ src/main/java/dev/shortlink/access/AccessController.java
@@ -1,16 +1,36 @@
 package dev.shortlink.access;
 
+import dev.shortlink.link.Link;
+import dev.shortlink.link.LinkRepository;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
 public class AccessController {
-    
+
+    private final LinkRepository linkRepository;
+
+    public AccessController(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
     @GetMapping("/{id}")
-    public String getCategoryById(@PathVariable String id) {
-        return "access " + id;
+    public void redirectLink(@PathVariable String shortUrl, HttpServletResponse response) throws IOException {
+        Optional<Link> linkOptional = linkRepository.findByShortUrl(Long.valueOf(shortUrl));
+
+        if (linkOptional.isPresent()) {
+            Link link = linkOptional.get();
+            response.sendRedirect(link.getOriginUrl());
+        } else {
+            response.sendError(HttpStatus.NOT_FOUND.value(), "Link not found");
+        }
     }
 }
--- src/main/java/dev/shortlink/link/LinkRepository.java
+++ src/main/java/dev/shortlink/link/LinkRepository.java
@@ -1,7 +1,11 @@
 package dev.shortlink.link;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
+
+    Optional<Link> findByShortUrl(Long shortUrl);
 
 }
